7fb207667520b28ec30c10484eef811bd18593ae
Dennis Kariuki Muiga <denkarish@gmail.com>
2025-07-05 11:10:57 +0300

    added models

--- hr_assistant/settings.py
+++ hr_assistant/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'notification'
+    'notification',
+    'leave',
+    'hrdocument',
 ]
 
 MIDDLEWARE = [
