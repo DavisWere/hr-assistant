17594fc2813d04e60de3fc1031b38c96af4ff7d2
Dennis Kariuki Muiga <denkarish@gmail.com>
2025-07-05 09:48:26 +0300

    `Added custom user model and app, updated settings and admin to use it.`

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    first_name = models.CharField(max_length=40, null=False)
+    last_name = models.CharField(max_length=40, null=False)
+    department = models.CharField(max_length=255, null=False)
+    role = models.CharField(max_length=255, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)  # Sets when created
+    updated_at = models.DateTimeField(auto_now=True)      # Updates on save
+
+    def __str__(self):
+        return self.username
