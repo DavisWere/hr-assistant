40475073af8a00381c68f2f6dfc6ef7cb309e6a3
Davisetims <etimsintegration@gmail.com>
2025-07-05 12:10:17 +0300

    "Added ChatHistoryViewSet and registered it to notification_router."

--- notification/urls.py
+++ notification/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
-from notification.views import MistralChatView
+from notification.views import MistralChatView, ChatHistoryViewSet
 
 notification_router= DefaultRouter()
 
+notification_router.register(r'chat/history', ChatHistoryViewSet)
 
 urlpatterns = [ 
     path('chat/', MistralChatView.as_view(), name='chat-with-mistral'),  
